65eb7588aef5402059d06d43cdd2b0ef36d128d0
Franz Holzinger <franz.holzinger@festwein.de>
2024-01-11 20:49:47 +0100

    Icons for Wizard

---
+++ Configuration/Icons.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'chgallery-plugin' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        // The source bitmap file
+        'source' => 'EXT:chgallery/Resources/Public/Icons/Wizard.gif'
+    ],
+];
